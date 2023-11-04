41dbce758dd16ea5221ea0bfccdcdcc5f909de45
Mahesh Kulkarni <maheshkulkarni@Maheshs-MacBook-Pro.local>
2023-11-04 21:59:10 +0530

    operator and condition

---
+++ Day3.playground/Contents.swift
@@ -0,0 +1,22 @@
+import UIKit
+
+var greeting = "Hello, playground"
+
+var name = "Mahesh"
+
+name += " Kulkarni"
+
+var range = 1...5
+
+var range2 = 1..<5
+
+for item in 1...5 {
+    print(item)
+}
+
+for item in 1..<5 {
+    print(item)
+}
+
+var array = [5,4,3,2,1]
+
